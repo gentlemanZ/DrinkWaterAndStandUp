5d85328250812d9ed41a1dbca741c5a4e5451d58
Tianyang Zhong <tianyangzhong2013@gmail.com>
2019-09-22 17:05:27 -0400

    trying out RxJava and count down timer.
    Signed-off-by: Tianyang Zhong <tianyangzhong2013@gmail.com>

--- app/src/main/java/com/tianyang/android/drinkwaterandstandup/viewModels/MainActivityViewModel.kt
+++ app/src/main/java/com/tianyang/android/drinkwaterandstandup/viewModels/MainActivityViewModel.kt
@@ -1,13 +1,24 @@
 package com.tianyang.android.drinkwaterandstandup.viewModels
 
 import android.arch.lifecycle.MutableLiveData
+import io.reactivex.Observable
+import java.util.concurrent.TimeUnit
 
 class MainActivityViewModel {
+    private val countDownTimerObserble: Observable<Long> = Observable
+        .interval(1, TimeUnit.SECONDS)
+        .map {
+            it
+        }
     val helloWorldLiveData: MutableLiveData<String> by lazy {
         MutableLiveData<String>()
     }
 
     init {
-        helloWorldLiveData.postValue("hello world")
+        countDownTimerObserble
+            .doOnEach {
+                helloWorldLiveData.postValue(it.value.toString())
+            }
+            .subscribe()
     }
 }
