dcac68f88a75bf2c6298fb9118bab0d5bfbf7b03
Tianyang Zhong <tianyangzhong2013@gmail.com>
2019-09-28 14:20:31 -0400

    fix the build and vm
    Signed-off-by: Tianyang Zhong <tianyangzhong2013@gmail.com>

--- app/src/main/java/com/tianyang/android/drinkwaterandstandup/viewModels/FragmentTimerConfigViewModel.kt
+++ app/src/main/java/com/tianyang/android/drinkwaterandstandup/viewModels/FragmentTimerConfigViewModel.kt
@@ -1,3 +1,14 @@
 package com.tianyang.android.drinkwaterandstandup.viewModels
 
-class FragmentTimerConfigViewModel
+import androidx.lifecycle.MutableLiveData
+import com.tianyang.android.drinkwaterandstandup.util.CountDownTimerUtil
+
+class FragmentTimerConfigViewModel {
+    val helloWorldLiveData: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    init {
+        CountDownTimerUtil.startCountDownTimer(helloWorldLiveData, 30)
+    }
+}
